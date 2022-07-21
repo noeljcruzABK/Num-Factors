774d358718aca0103d1dd08aca8d379a6125616e
Noel Cruz <nocruz@activision.com>
2022-07-21 10:42:22 -0400

    finished up getFactors function. Returns list of factors for the number entered.

--- Num-Factors/Num-Factors.cpp
+++ Num-Factors/Num-Factors.cpp
@@ -2,7 +2,24 @@
 // Take an input of a positive value and write back all of the values factors.
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+string getFactors(int num) {
+	string factorList = "";
+
+	for (int i = 1; i <= num; i++) {
+		string factor = to_string(i);
+
+		if (num == i) {
+			factorList.append("and " + factor + ".");
+		}
+		else if (num % i == 0) {
+			factorList.append(factor + ", ");
+		}
+	}
+	return factorList;
+}
 
 int main()
 {
@@ -11,5 +28,5 @@ int main()
 	cout << "- Factors of a Number -" << endl;
 	cout << "Please enter a positive, whole number to see a list of its factors:" << endl;
 	cin >> input;
-	cout << input;
+	cout << "The factors of " << input << " are: " << getFactors(input) << endl;
 }
